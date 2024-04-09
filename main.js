8d21057b97e78edbb3a45f1723c5ea3232007ddd
MehjabinKhan <161743992+MehjabinKhan@users.noreply.github.com>
2024-04-09 17:34:42 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+"use strict";
+let favouriteFood = "Briyani";
+console.log(favouriteFood);
+favouriteFood = "Karahi";
+console.log(favouriteFood);
+let yourName = "Mehjabin";
+let surName = "Khan";
+console.log(`My name is ${yourName} ${surName}.`);
+let profession = ("Programmer");
+console.log(`Hey there! I'm a ${profession}.`);
+const yourAge = "20";
+const eyeColor = "brown";
+console.log(yourAge, eyeColor);
+const mathsTestMarks = 45;
+const scienceTestMarks = 50;
+const totalMarks = mathsTestMarks + scienceTestMarks;
+console.log(totalMarks);
